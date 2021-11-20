be020926af1bb3c778884552aa3591dd29d62935
Ethan <53139397+Ethan-02@users.noreply.github.com>
2021-11-20 11:11:06 +0800

    Update matplotlib_15_1.py

--- 第15章 生成数据/matplotlib_15_1.py
+++ 第15章 生成数据/matplotlib_15_1.py
@@ -1,12 +1,13 @@
+#生成显示1~5000的整数的立方的图表
 import matplotlib.pyplot as plt
 
-intput_values = list(range(1, 5000))
-squares = [x**3 for x in intput_values]
+intput_values = list(range(1, 5000))#创建1~5000的数字列表
+cube = [x**3 for x in intput_values]#根据intput_values列表生成三次方列表
 
-plt.plot(intput_values, squares, linewidth = 5)
-plt.title("Numbers", fontsize = 24)
-plt.xlabel("Value", fontsize = 14)
-plt.ylabel("三次方", fontsize = 14)
+plt.plot(intput_values, cube, linewidth = 5)
+plt.title("Cube Numbers", fontsize = 24)#图表的标题
+plt.xlabel("Value", fontsize = 14)#横坐标名称
+plt.ylabel("Cube of Value", fontsize = 14)#纵坐标名称
 
-plt.tick_params(axis='both', labelsize=14)
-plt.show()
+plt.tick_params(axis='both', labelsize=14)#设置刻度标记的字号
+plt.show()#打开matplotlib查看器
