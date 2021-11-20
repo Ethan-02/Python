11ee6842f11b082b37046f73facf8d6904f99789
Ethan <53139397+Ethan-02@users.noreply.github.com>
2021-11-20 11:16:05 +0800

    Update matplotlib_15_2.py

--- 第15章 生成数据/matplotlib_15_2.py
+++ 第15章 生成数据/matplotlib_15_2.py
@@ -1,13 +1,14 @@
+#作业15.1的颜色映射
 import matplotlib.pyplot as plt
 
 x_values = list(range(1, 5000))
 y_values = [x**3 for x in x_values]
 
-plt.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Blues , s = 40)
+plt.scatter(x_values, y_values, c = y_values, cmap=plt.cm.Blues , s = 40)#绘制散点图并生成颜色映射
 plt.title("Cube Numbers", fontsize = 24)
 plt.xlabel("Value", fontsize = 14)
 plt.ylabel("Cube of Value", fontsize = 14)
 
 plt.tick_params(axis='both', labelsize=14)
-plt.savefig('cube_plot.png', bbox_inches='tight')
+plt.savefig('cube_plot.png', bbox_inches='tight')#将图表保存为cube_plot.png
 plt.show()
